8c80073599f3253ce3dd4cf4b6471b0b91053a33
KesvaL <jabeddou@gmail.com>
2023-05-21 23:54:48 +0200

    :tada: First commit!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Chessground } from 'svelte-chessground';
+</script>
+
+<main>
+	<Chessground board />
+</main>
+
+<style lang="scss">
+	main {
+		max-width: 40vw;
+	}
+</style>
